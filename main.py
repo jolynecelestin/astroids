4bbde2481d873afb3a40565976d5206ae470d7f2
jolyne_celestin <jolynecelestin@gmail.com>
2025-06-29 12:21:05 -0400

    Limit/Kill/Split

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            for bullet in shots:
+                if asteroid.collides_with(bullet):   
+                    bullet.kill()
+                    asteroid.split()
+       
 
         pygame.Surface.fill(screen, (0, 0, 0))
         
